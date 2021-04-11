1d1ffaaf5661d17ce36468ff7cfef1b128d37d8a
sanjuGoudru <sanjugr7022@gmail.com>
2021-04-11 13:07:27 +0530

    Update app.py

--- Deployment/app.py
+++ Deployment/app.py
@@ -9,7 +9,7 @@ from flask import Flask, render_template,url_for,request
 import pandas as pd
 import pickle
 
-xgb_model = pickle.load(open('./Models/xgb_regression.pkl','rb'))
+xgb_model = pickle.load(open('../Models/xgb_regression.pkl','rb'))
 
 app = Flask(__name__)
 
@@ -18,12 +18,12 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/predict',method=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    df = pd.read_csv('./Data/Real_Data/real_2018.csv')
+    df = pd.read_csv('../Data/Real_Data/real_2018.csv')
     my_pred = xgb_model.predict(df.iloc[:,:-1])
     my_pred = my_pred.tolist()
-    return render_template('return.html',prediction=my_pred)
+    return render_template('result.html',prediction=my_pred)
 
 if __name__ == '__main__':
     app.run(debug=True)
